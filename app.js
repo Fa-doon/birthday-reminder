eec43055475c4e333bb749320bb788efd5b172d3
Fadoon T <fadoon.tar@gmail.com>
2024-04-07 15:52:00 +0100

    time adjustments

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ const transporter = nodemailer.createTransport({
 });
 
 // Scheduling job to send birthday emails by 7am
-const job = schedule.scheduleJob("53 15 * * *", async () => {
+const job = schedule.scheduleJob("58 15 * * *", async () => {
   logger.info("Job is running");
   const today = new Date();
   today.setHours(0, 0, 0, 0);
